994b8d05d92d7b6c660f6b7c3e466f65151b775e
Josh Blum <josh@joshknows.com>
2015-11-21 19:56:37 -0800

    gr-pothos: workaround for missing public header include

---
+++ missing/include/README.txt
@@ -0,0 +1,1 @@
+workaround for missing public header include
---
+++ missing/lib/codec2/codec2.h
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////
+// Copied defs from the real codec 2 to fix missing GNU Radio header bug
+////////////////////////////////////////////////////////////////////////
+
+/*---------------------------------------------------------------------------*\
+
+  FILE........: codec2.h
+  AUTHOR......: David Rowe
+  DATE CREATED: 21 August 2010
+
+  Codec 2 fully quantised encoder and decoder functions.  If you want use
+  Codec 2, these are the functions you need to call.
+
+\*---------------------------------------------------------------------------*/
+
+/*
+  Copyright (C) 2010 David Rowe
+
+  All rights reserved.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License version 2.1, as
+  published by the Free Software Foundation.  This program is
+  distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define CODEC2_MODE_3200 0
+#define CODEC2_MODE_2400 1
+#define CODEC2_MODE_1600 2
+#define CODEC2_MODE_1400 3
+#define CODEC2_MODE_1300 4
+#define CODEC2_MODE_1200 5
